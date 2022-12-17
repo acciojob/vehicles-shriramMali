cb1dc46f268d80e35581da49c47cdffd7fe1ece5
malishrirama17@gmail.com <you@example.com>
2022-12-17 15:24:59 +0530

    new

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,22 @@
+package com.driver;
+
+class Boat implements WaterVehicle {
+    String name;
+    int capacity;
+    Boat(String name,int capacity){
+        this.name=name;
+        this.capacity=capacity;
+    }
+    @Override
+    public String getVehicleName() {
+        return name;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+}
+
+
+
--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -4,16 +4,4 @@ public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
 }
- class Boat implements WaterVehicle{
-    String name;
-    int capacity;
-    @Override
-    public String getVehicleName() {
-        return name;
-    }
 
-    @Override
-    public int getVehicleCapacity() {
-        return capacity;
-    }
-}
