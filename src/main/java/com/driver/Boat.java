578d9dc97f7d98ff557c86d6bed181921544ac69
malishrirama17@gmail.com <you@example.com>
2022-12-17 15:35:21 +0530

    msg

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,6 +1,6 @@
 package com.driver;
 
-class Boat implements WaterVehicle {
+public class Boat implements WaterVehicle {
     String name;
     int capacity;
     Boat(String name,int capacity){
@@ -9,12 +9,12 @@ class Boat implements WaterVehicle {
     }
     @Override
     public String getVehicleName() {
-        return name;
+        return this.name;
     }
 
     @Override
     public int getVehicleCapacity() {
-        return capacity;
+        return this.capacity;
     }
 }
 
