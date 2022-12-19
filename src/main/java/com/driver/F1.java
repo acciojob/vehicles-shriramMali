42eff7bf4352df2a382e2674ec18037af88f1b71
malishrirama17@gmail.com <you@example.com>
2022-12-19 18:11:11 +0530

    s

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -25,28 +25,23 @@ public class F1 extends Car {
         //for all other cases, change the gear accordingly
 
         if(newSpeed > 0) {
-            if( newSpeed <=50 ){
-               changeGear(1);
-            }
-            else if( newSpeed <=100 ){
+            if (newSpeed <= 50) {
+                changeGear(1);
+            } else if (newSpeed <= 100) {
                 changeGear(2);
-            }
-            else if( newSpeed <=150 ){
-               changeGear(3);
-            }
-            else if(newSpeed <=200 ){
+            } else if (newSpeed <= 150) {
+                changeGear(3);
+            } else if (newSpeed <= 200) {
                 changeGear(4);
-            }
-            else if( newSpeed <=250 ){
-              changeGear(5);
-            }
-            else { changeGear(6);
+            } else if (newSpeed <= 250) {
+                changeGear(5);
+            } else {
+                changeGear(6);
 
             }
-
+        }
             if(newSpeed > 0) {
                 changeSpeed(newSpeed, getCurrentDirection());
             }
-        }
     }
 }
